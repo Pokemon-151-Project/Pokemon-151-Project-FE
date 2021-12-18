9eb5759f7715d941e01de4272c89013b2ab6c85f
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-17 22:36:30 -0500

    ready to push to main

--- pokemon-project/src/components/AllPokemon.js
+++ pokemon-project/src/components/AllPokemon.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 const AllPokemon = (props) =>{
     const {pokemon} = props;
-
-    console.log('AllPokemon pokemon:', pokemon)
-    return(
-        <div>Blah Blah Blah</div>
-    )
+    return(<div>This is a PokeMon container</div>)
 };
 
 export default AllPokemon;
