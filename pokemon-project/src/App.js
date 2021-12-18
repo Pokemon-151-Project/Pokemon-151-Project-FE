63f0955c27aab0e03341df602202e826443a91cb
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-17 22:22:00 -0500

    got pokeAPI call up and working, installed Axios, set the list of pokemon to state. Ready to push to main.

--- pokemon-project/src/App.js
+++ pokemon-project/src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+const [pokemon, setPokemon] = useState({});
+
+useEffect( () =>{
+  axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    .then(res =>{
+      console.log('Res.data.results:', res.data.results)
+      setPokemon(res.data.results)
+    })
+    .catch(err =>{
+      console.log('Error:', err)
+    })
+}, [])
+  
+console.log(pokemon[0])
+
+
   return (
     <div className="App">
       <header className="App-header">
