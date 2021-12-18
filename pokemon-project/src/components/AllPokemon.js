c22d432aaef3327f08869462b4534e569b716b06
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-17 22:31:16 -0500

    made AllPokemon component to contain all the individual pokemon containers.

---
+++ pokemon-project/src/components/AllPokemon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AllPokemon = (props) =>{
+    const {pokemon} = props;
+
+    console.log('AllPokemon pokemon:', pokemon)
+    return(
+        <div>Blah Blah Blah</div>
+    )
+};
+
+export default AllPokemon;
