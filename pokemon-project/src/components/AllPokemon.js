8e4baf14aa405dc95dcb88de7a4f79cb6d81ada0
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-23 21:29:48 -0500

    wrapped the return of AllPokemon in a div, then decided not to do that because it returned an error.

--- pokemon-project/src/components/AllPokemon.js
+++ pokemon-project/src/components/AllPokemon.js
@@ -6,11 +6,9 @@ import pokemon from '../data';
 const AllPokemon = () =>{
     console.log('Pokemon:', pokemon[0])
     return(
-        <div>
             pokemon.map(poke =>{
             return(<SinglePokemon poke = {poke} key = {poke.name}/>)
        })
-        </div>
         )
 };
 
