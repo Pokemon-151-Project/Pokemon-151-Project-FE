08b0415e4c9fc1df12f1f01ad50b80ce3cdf0f5f
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-23 22:05:44 -0500

    in SinglePokemon I switched the places of the img and the types

--- pokemon-project/src/components/SinglePokemon.js
+++ pokemon-project/src/components/SinglePokemon.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const SinglePokemon = props =>{
+    //Each poke is a single pokemon's data, being passed in as props.
     const {poke} = props;
 
     // console.log('SinglePOkemon props:', props)
@@ -10,11 +11,11 @@ const SinglePokemon = props =>{
             {/* Name of each pokemon */}
             <div>{poke.name}</div>
 
-            {/* image of each pokemon */}
-            <img src= {poke.img}/>
-
             {/* This returns basic type information. Singular if only one type, plural if two types. */}
             {poke.type[1]? <div>Types: {poke.type[0]} / {poke.type[1]} </div> : <div>Type: {poke.type}</div>}
+            
+            {/* image of each pokemon */}
+            <img src= {poke.img}/>
         </section>
     )
 }
