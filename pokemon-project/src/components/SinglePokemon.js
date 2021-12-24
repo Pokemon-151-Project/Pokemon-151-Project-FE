673a94bf679f84b916e0b7858d2938a58341627d
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-23 22:35:25 -0500

    making some progress on flexbox, but still have work to do

--- pokemon-project/src/components/SinglePokemon.js
+++ pokemon-project/src/components/SinglePokemon.js
@@ -7,16 +7,16 @@ const SinglePokemon = props =>{
     // console.log('SinglePOkemon props:', props)
     
     return (
-        <section className='pokemon-card'>
+        <div className='pokemon-card'>
             {/* Name of each pokemon */}
-            <div>{poke.name}</div>
+            <h2 className='poke-name'>{poke.name}</h2>
 
             {/* This returns basic type information. Singular if only one type, plural if two types. */}
             {poke.type[1]? <div>Types: {poke.type[0]} / {poke.type[1]} </div> : <div>Type: {poke.type}</div>}
 
             {/* image of each pokemon */}
             <img src= {poke.img}/>
-        </section>
+        </div>
     )
 }
 
