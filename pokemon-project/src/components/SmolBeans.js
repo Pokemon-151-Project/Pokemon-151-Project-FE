cbedbcd449b5633e440a7c0a1291fd97adc228c4
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-24 20:16:11 -0500

    added helpful comments

--- pokemon-project/src/components/SmolBeans.js
+++ pokemon-project/src/components/SmolBeans.js
@@ -4,16 +4,15 @@ import SinglePokemon from "./SinglePokemon";
 
 //This will be similar to AllPokemon but only display small ones, maybe less than 1kg? still figuring out how exactly to do this.
 
-const smolData =  pokemon.filter(item =>{
-    return item.type[0] === 'Fire'
-})
-console.log(smolData)
 
 const SmolBeans = props =>{
+    const smolData =  pokemon.filter(item =>{
+        return item.type[0] === 'Fire'
+    })
     // console.log('Pokemon:', pokemon[0])
         return(
            <section className='all-pokemon'>
-           { pokemon.map(poke =>{
+           { smolData.map(poke =>{
                 return(<SinglePokemon poke = {poke} key = {poke.name}/>)
            })}
         </section>
