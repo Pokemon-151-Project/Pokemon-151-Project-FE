25abef95ab5097478d5edfd04d179bc17ec5c50a
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-25 04:22:30 -0500

    got rid of data from adding dex entries

--- pokemon-project/src/App.js
+++ pokemon-project/src/App.js
@@ -10,27 +10,6 @@ import AllPokemon from './components/AllPokemon';
 import SmolBeans from './components/SmolBeans';
 
 function App() {
-
-//proof of concept for adding pokedex entries. I need to find an API with pokedex entries, and use this method to add said entries to data, then replace the original data.js with the new object.
-
-data.forEach(item =>{
-  let newData = data;
-  // console.log(item.name)
-
-  axios.get(`https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/pokedex.php?pokemon=${item.name}`)
-    .then(res =>{
-      console.log(res.data.info.id)
-      console.log('Data with id:', newData[res.data.info.id -1]) 
-      newData[res.data.info.id -1].dexEntry = res.data.info.description;
-      // console.log('newData:', newData)
-      console.log('New Data:', newData)
-    })
-    .catch(err=>{
-      console.log(err)
-    });
-})
-
-
   return (
     <div className="App">
       <Header/>
