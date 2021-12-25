1eb86f079a33eb45fa59c4e1b137666a5996ae1b
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-24 20:20:26 -0500

    more helpful comments

--- pokemon-project/src/components/SmolBeans.js
+++ pokemon-project/src/components/SmolBeans.js
@@ -4,12 +4,13 @@ import SinglePokemon from "./SinglePokemon";
 
 //This will be similar to AllPokemon but only display small ones, maybe less than 1kg? still figuring out how exactly to do this.
 
+const smolData =  pokemon.filter(item =>{
+    return item.type[0] === 'Fire'
+})
+// console.log('Pokemon:', pokemon[0]) 
 
 const SmolBeans = props =>{
-    const smolData =  pokemon.filter(item =>{
-        return item.type[0] === 'Fire'
-    })
-    // console.log('Pokemon:', pokemon[0])
+    //This is just a prototype proof of concept filter. It does display only fire types. So for each function I should be able to change the filter to whatever I want, and copy-paste. Or maybe I can do this even more smartly? It wouldn't be very DRY to write a slightly different component for each filter option, hmm.
         return(
            <section className='all-pokemon'>
            { smolData.map(poke =>{
