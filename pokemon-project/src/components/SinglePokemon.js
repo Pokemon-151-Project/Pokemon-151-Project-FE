b9fe29b31644b70d8ba003241f322d87da74e30e
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-25 04:44:20 -0500

    just decided to display dex entry in normal card

--- pokemon-project/src/components/SinglePokemon.js
+++ pokemon-project/src/components/SinglePokemon.js
@@ -10,13 +10,13 @@ const SinglePokemon = props =>{
             <h2 className='poke-name'>{poke.name}</h2>
 
             {/* This returns basic type information. Singular if only one type, plural if two types. */}
-            {poke.type[1]? <p>Types: {poke.type[0]} / {poke.type[1]} </p> : <p>Type: {poke.type}</p>}
+            {poke.type[1]? <h3>Types: {poke.type[0]} / {poke.type[1]} </h3> : <h3>Type: {poke.type}</h3>}
 
             {/* image of each pokemon */}
             <img  className= 'poke-img' src= {poke.img}/>
 
             {/* I now have added pokedex entries to each pokemon. Will uncomment this when I"ve added styling to this bit. Specifically, I only want a preview to show up, then the full entry when you hover over it.*/}
-            {/* <p>{poke.dexEntry}</p> */}
+            <p>{poke.dexEntry}</p>
         </div>
     )
 }
