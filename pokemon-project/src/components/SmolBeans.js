950289207fa4f5b1d443e45f4c255d8c43aff8db
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-24 20:34:04 -0500

    done for now

--- pokemon-project/src/components/SmolBeans.js
+++ pokemon-project/src/components/SmolBeans.js
@@ -1,5 +1,7 @@
 import React from "react";
+import App from "../App";
 import pokemon from "../data";
+import AllPokemon from "./AllPokemon";
 import SinglePokemon from "./SinglePokemon";
 
 //This will be similar to AllPokemon but only display small ones, maybe less than 1kg? still figuring out how exactly to do this.
@@ -10,7 +12,9 @@ const smolData =  pokemon.filter(item =>{
 // console.log('Pokemon:', pokemon[0]) 
 
 const SmolBeans = props =>{
-    //This is just a prototype proof of concept filter. It does display only fire types. So for each function I should be able to change the filter to whatever I want, and copy-paste. Or maybe I can do this even more smartly? It wouldn't be very DRY to write a slightly different component for each filter option, hmm.
+    //This is just a prototype proof of concept filter. It successfully displays only fire types. But the issue is, as it stands I have to make a bunch of slightly-different components each with a unique filter. I think maybe I can just use one mega-component and change the data passed in through props. Will probably use that by setting the data to State in App.js, then set State with functions based on what filter the user clicks.
+        // -Maybe use State in App.js to determine which props to pass in, and that can tell AllPokemon.js which pokes to display?
+
         return(
            <section className='all-pokemon'>
            { smolData.map(poke =>{
