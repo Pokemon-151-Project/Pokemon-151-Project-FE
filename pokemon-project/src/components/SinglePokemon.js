05129a893a80f4d7148297794e3d79c3ddc84919
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-26 13:44:34 -0500

    destructured the keys in SinglePokemon to make my JSX a little more concise

--- pokemon-project/src/components/SinglePokemon.js
+++ pokemon-project/src/components/SinglePokemon.js
@@ -4,6 +4,9 @@ const SinglePokemon = (props) => {
   //Each poke is a single pokemon's data, being passed in as props.
   const { poke } = props;
 
+  //destructuring the keys in poke to make my JSX a little more concise
+  const { type, img, dexEntry } = poke;
+
   return (
     <div className="pokemon-card">
       {/* Name of each pokemon */}
@@ -12,17 +15,17 @@ const SinglePokemon = (props) => {
       {/* This returns basic type information. Singular if only one type, plural if two types. */}
       {poke.type[1] ? (
         <h3>
-          Types: {poke.type[0]} / {poke.type[1]}{" "}
+          Types: {type[0]} / {type[1]}{" "}
         </h3>
       ) : (
-        <h3>Type: {poke.type}</h3>
+        <h3>Type: {type}</h3>
       )}
 
       {/* image of each pokemon */}
-      <img className="poke-img" src={poke.img} alt="Pokemon" />
+      <img className="poke-img" src={img} alt="Pokemon" />
 
       {/* I now have added pokedex entries to each pokemon. Will uncomment this when I"ve added styling to this bit. Specifically, I only want a preview to show up, then the full entry when you hover over it.*/}
-      <p>{poke.dexEntry}</p>
+      <p>{dexEntry}</p>
     </div>
   );
 };
