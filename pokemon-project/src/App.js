91e8ee5c4a61f9d8048a897752adf025dc004525
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-25 21:08:36 -0500

    commented out my not-yet-successful state changing work and stored it at the bottom of App.js

--- pokemon-project/src/App.js
+++ pokemon-project/src/App.js
@@ -12,22 +12,6 @@ function App() {
   //sets the pokemon data to state. It starts as the original data but my hope is that I can update it based on filters.
   const [pokemon, setPokemon] = useState(data);
 
-  const setSmall = async () => {
-    const newData = await pokemon.filter(async (item) => {
-      const weightInt = item.weight.split(" ")[0];
-      // console.log(weightInt);
-      // console.log(item.weight);
-      return weightInt < 5;
-    });
-    console.log("afjdsalfsd");
-    setPokemon(newData);
-  };
-
-  setSmall();
-  // useEffect(() => {
-  //   setSmall();
-  // }, []);
-
   return (
     <div className="App">
       <Header />
@@ -44,3 +28,23 @@ export default App;
 //It'll display whatever pokes I want to sort by, passed in through props from App
 //What pokes it takes in as props is set by the user clicking buttons in Header
 //Maybe even have just a few catch-all functions to set state, rather than say, one for each type. Like the "type" catch-all function would take in a type and set the pokemon data to only pokemon of that type.
+
+//***********SAVING CARD DISPLAY FUNCTIONS HERE:
+
+//Here's some of my work on trying to set the display options to state. This will go in App.js I think Haevn't quite gotten there, calling setSmall() makes it render endlessly. Will maybe need to ask for help.
+// const setSmall = async () => {
+//   const newData = await pokemon.filter(async (item) => {
+//     const weightInt = item.weight.split(" ")[0];
+//     // console.log(weightInt);
+//     // console.log(item.weight);
+//     return weightInt < 5;
+//   });
+//   console.log("afjdsalfsd");
+//   setPokemon(newData);
+// };
+
+// setSmall();
+// useEffect(() => {
+//   setSmall();
+// }, []);
+//****************END SAVING CARD DISPLAY OPTIONS
