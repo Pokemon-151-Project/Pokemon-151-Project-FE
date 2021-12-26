482eddf6983863ae77f19f86afae349c86500c54
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-25 20:29:03 -0500

    adding helpful comments to App.js

--- pokemon-project/src/App.js
+++ pokemon-project/src/App.js
@@ -18,3 +18,9 @@ function App() {
 }
 
 export default App;
+
+//So, my hope here for display options is to do something like this:
+//Make <AllPokemon/> a catch-all component
+//It'll display whatever pokes I want to sort by, passed in through props from App
+//What pokes it takes in as props is set by the user clicking buttons in Header
+//Maybe even have just a few catch-all functions to set state, rather than say, one for each type. Like the "type" catch-all function would take in a type and set the pokemon data to only pokemon of that type.
