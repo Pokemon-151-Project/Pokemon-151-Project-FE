d64bb9d9386fee7bd0b7c4cf5ea32a459c29aea0
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-25 20:23:27 -0500

    got weight filter function up and running for SmolBeans. Hoping this will be a template function eventually so I can keep things DRY.

--- pokemon-project/src/App.js
+++ pokemon-project/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 //importing components
 import Header from "./components/Header";
 import AllPokemon from "./components/AllPokemon";
+import SmolBeans from "./components/SmolBeans";
 
 import pokemon from "./data";
 
@@ -10,8 +11,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <AllPokemon pokemon={pokemon} />
-      {/* <SmolBeans/> */}
+      {/* <AllPokemon pokemon={pokemon} /> */}
+      <SmolBeans pokemon={pokemon} />
     </div>
   );
 }
--- pokemon-project/src/components/SmolBeans.js
+++ pokemon-project/src/components/SmolBeans.js
@@ -1,5 +1,5 @@
 import React from "react";
-import pokemon from "../data";
+// import pokemon from "../data";
 import SinglePokemon from "./SinglePokemon";
 
 //This will be similar to AllPokemon but only display small ones, maybe less than 1kg? still figuring out how exactly to do this.
@@ -10,10 +10,11 @@ const SmolBeans = (props) => {
   const { pokemon } = props;
 
   const smolData = pokemon.filter((item) => {
-    return item.type[0] === "Fire";
+    const weightInt = item.weight.split(" ")[0];
+    console.log(weightInt);
+    // console.log(item.weight);
+    return weightInt < 5;
   });
-  //This is just a prototype proof of concept filter. It successfully displays only fire types. But the issue is, as it stands I have to make a bunch of slightly-different components each with a unique filter. I think maybe I can just use one mega-component and change the data passed in through props. Will probably use that by setting the data to State in App.js, then set State with functions based on what filter the user clicks.
-  // -Maybe use State in App.js to determine which props to pass in, and that can tell AllPokemon.js which pokes to display?
 
   return (
     <section className="all-pokemon">
