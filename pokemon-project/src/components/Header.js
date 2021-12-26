a1ae51b6af3573b558a0f827e1ac3982c57d0783
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-25 23:32:20 -0500

    fixed small bug due to typo

--- pokemon-project/src/components/Header.js
+++ pokemon-project/src/components/Header.js
@@ -1,11 +1,5 @@
 import React from "react";
 
-const Header = () => {
-  return (
-    <header>
-      <h1>Pokemon Display</h1>
-      import React from "react";
-
 const Header = (props) => {
   const { setSmall } = props;
   return (
@@ -35,30 +29,6 @@ const Header = (props) => {
                     <a href='#'>Big Pokemon</a>
                     <a href='#'>Sort by type</a> */}
           <button onClick={setSmall}>Smol Beans</button>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
-      {/* section for info about me */}
-      <div className="adam-info">
-        <h2>Author: Adam Hinton</h2>
-
-        <a href="https://github.com/adamhinton/pokemon-project" target="_blank">
-          Source
-        </a>
-      </div>
-
-      {/* dropdown menu for display options */}
-      <div className="dropdown">
-        <button className="dropbtn">Display Options (check this out!</button>
-
-        <div className="dropdown-content">
-          <a href="#">Tiny Pokemon</a>
-          <a href="#">Big Pokemon</a>
-          <a href="#">Sort by type</a>
         </div>
       </div>
     </header>
