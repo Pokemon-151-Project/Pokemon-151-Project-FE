10bbbdca6ad5c1d5e0d8d06d4413b36fb62c39c5
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-27 13:57:33 -0500

    changed width on hover to 1.35 rem

--- pokemon-project/src/components/SinglePokemon.js
+++ pokemon-project/src/components/SinglePokemon.js
@@ -10,7 +10,7 @@ const SinglePokemon = (props) => {
   return (
     <div className="pokemon-card">
       {/* Pokemon's name */}
-      <h2 className="poke-name">{poke.name}</h2>
+      <h2 className="poke-name card-item">{poke.name}</h2>
 
       {/* This returns basic type information. Singular if only one type, plural if two types. */}
       {poke.type[1] ? (
@@ -18,19 +18,19 @@ const SinglePokemon = (props) => {
           Types: {type[0]} / {type[1]}{" "}
         </h3>
       ) : (
-        <h3>Type: {type}</h3>
+        <h3 className="card-item">Type: {type}</h3>
       )}
 
-      <h4 className="height-weight">
+      <h4 className="height-weight card-item">
         Height: {height} <br />
         Weight: {weight}
       </h4>
 
       {/* image of each pokemon */}
-      <img className="poke-img" src={img} alt="Pokemon" />
+      <img className="poke-img card-item" src={img} alt="Pokemon" />
 
       {/* I now have added pokedex entries to each pokemon. Will uncomment this when I"ve added styling to this bit. Specifically, I only want a preview to show up, then the full entry when you hover over it.*/}
-      <p>{dexEntry}</p>
+      <p className="card-item">{dexEntry}</p>
     </div>
   );
 };
