841f2e680a3f161604869b2829956a4df66f09d5
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-28 12:35:00 -0500

    App.test.js now tests to make sure child components show up

--- pokemon-project/src/App.test.js
+++ pokemon-project/src/App.test.js
@@ -7,11 +7,17 @@ test("renders title", () => {
   render(<App />);
   const linkElement = screen.getByText(/Pokemon Display/i);
   expect(linkElement).toBeInTheDocument();
+});
 
+test("renders child components", () => {
+  render(<App />);
   const docHeader = document.querySelector("header");
   expect(docHeader).toBeInTheDocument();
 
   const h1 = screen.getByText(/pokemon display/i);
   console.debug("h1:", h1);
   expect(h1).toBeInTheDocument();
+
+  const bulba = screen.getByText(/bulbasaur/i);
+  expect(bulba).toBeInTheDocument();
 });
