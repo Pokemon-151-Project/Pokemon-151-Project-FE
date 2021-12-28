4a3f60714324687ffc7fb3c40bc2b0ba7636b77a
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-28 12:11:35 -0500

    already getting errors from vs code when I try to make tests

--- pokemon-project/src/App.test.js
+++ pokemon-project/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders title", () => {
   render(<App />);
   const linkElement = screen.getByText(/Pokemon Display/i);
+
+  const header = document.querySelector("header");
+
   expect(linkElement).toBeInTheDocument();
 });
