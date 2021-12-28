86b1280ca8f2c56db5ce2f4ac80941f2bb488cb6
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-28 12:19:37 -0500

    installed react testing (I think )

--- pokemon-project/src/App.test.js
+++ pokemon-project/src/App.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-node-access */
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
@@ -5,8 +6,11 @@ import App from "./App";
 test("renders title", () => {
   render(<App />);
   const linkElement = screen.getByText(/Pokemon Display/i);
-
-  const header = document.querySelector("header");
-
   expect(linkElement).toBeInTheDocument();
+
+  const docHeader = document.querySelector("header");
+  expect(docHeader).toBeInTheDocument();
+
+  const dropdown = document.getElementsByClassName("dropdown");
+  userEvent.click(dropdown);
 });
