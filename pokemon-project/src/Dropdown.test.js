5deff5b010da4ad6e2f13b144e747e43c345d19d
adamhinton <adam.d.hinton@gmail.com>
2021-12-29 02:58:06 -0500

    git message test

--- pokemon-project/src/Dropdown.test.js
+++ pokemon-project/src/Dropdown.test.js
@@ -1,9 +1,8 @@
 import Dropdown from "./components/Dropdown";
 
 /* eslint-disable testing-library/no-node-access */
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
 
 test("component renders without errors", () => {
   render(<Dropdown />);
