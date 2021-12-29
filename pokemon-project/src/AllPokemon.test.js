450c7485236aea775a63705079737b952ca35042
adamhinton <adam.d.hinton@gmail.com>
2021-12-29 13:43:03 -0500

    added helpful comments

--- pokemon-project/src/AllPokemon.test.js
+++ pokemon-project/src/AllPokemon.test.js
@@ -4,5 +4,6 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 test("AllPokemon renders without errors", () => {
+  //returning undefined for now until I figure out how to define pokemon
   return undefined;
 });
