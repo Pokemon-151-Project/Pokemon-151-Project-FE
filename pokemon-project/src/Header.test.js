a408e5e49c43df7a40738fbb274db5b02f9af099
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-28 21:10:29 -0500

    added testing to make sure dropdown shows up when clicked

--- pokemon-project/src/Header.test.js
+++ pokemon-project/src/Header.test.js
@@ -3,13 +3,13 @@ import Header from "./components/Header";
 /* eslint-disable testing-library/no-node-access */
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
-test("Renders without errors", () => {
+test("Component renders without errors", () => {
   render(<Header />);
 });
 
-test("Renders the expect segments of the header", () => {
+test("Renders the expected segments of the header", () => {
   render(<Header />);
   const h1 = screen.queryByText(/pokemon display/i);
   const author = screen.queryByText(/author: adam hinton/i);
@@ -20,4 +20,17 @@ test("Renders the expect segments of the header", () => {
   expect(h1).toBeInTheDocument();
   expect(source).toBeInTheDocument();
   expect(dropbtn).toBeVisible();
+});
+
+test("dropdown only appears when you click dropdown button", () => {
+  render(<Header />);
+  //This test renders the dropdown content by clicking on the dropdown button, then make sure the appropriate content shows up.
+
+  const dropbtn = screen.queryByText(/display options/i);
+  expect(dropbtn).toBeInTheDocument();
+
+  userEvent.click(dropbtn);
+
+  const dropdownContent = screen.queryByText(/pick a type/i);
+  expect(dropdownContent).toBeInTheDocument();
 });
