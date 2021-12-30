a3e657668563648e596b30288605326cda916f0e
adamhinton <adam.d.hinton@gmail.com>
2021-12-29 21:27:48 -0500

    Added in render and rerender testing for AllPokemo

--- pokemon-project/src/AllPokemon.test.js
+++ pokemon-project/src/AllPokemon.test.js
@@ -1,18 +1,23 @@
 import AllPokemon from "./components/AllPokemon";
-// import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-const fakePokemon = {
-  name: "Pokeman",
-  type: "Death Type",
-  img: "https://google.com",
-  dexEntry: "blah blah blah",
-  height: "4m",
-  weight: "50kg",
-};
+const fakePokemon = [
+  {
+    name: "Pokeman",
+    type: "Death Type",
+    img: "https://google.com",
+    dexEntry: "blah blah blah",
+    height: "4m",
+    weight: "50kg",
+  },
+];
 
 //NEED TO UN-COMMENT RENDER AND SCREEN AND ALLpOKEMON IMPORTS
-test("AllPokemon renders without errors", () => {
+test("AllPokemon renders without errors with no props, then rerenfers with correct props", () => {
+  const { rerender } = render(<AllPokemon pokemon={[]} />);
+
+  rerender(<AllPokemon pokemon={fakePokemon} />);
   //returning undefined for now until I figure out how to define pokemon
   return undefined;
 });
