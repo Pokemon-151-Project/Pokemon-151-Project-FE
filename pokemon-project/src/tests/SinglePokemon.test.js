346e392907d5c8cc1bd00e5f7f5029f7cae10183
adamhinton <adam.d.hinton@gmail.com>
2021-12-29 22:20:39 -0500

    Testing to make sure correct info displays in SinglePOkemon

--- pokemon-project/src/tests/SinglePokemon.test.js
+++ pokemon-project/src/tests/SinglePokemon.test.js
@@ -16,3 +16,19 @@ const fakePokemon = [
 test("renders without errors", () => {
   render(<SinglePokemon poke={fakePokemon[0]} />);
 });
+
+test("Displays correct info on screen", () => {
+  render(<SinglePokemon poke={fakePokemon[0]} />);
+
+  const name = screen.queryByText(/fake pokemon name/i);
+  const type = screen.queryByText(/grass/i);
+  const dexEntry = screen.queryByText(/blah blah blah/i);
+  const height = screen.queryByText(/4m/i);
+  const weight = screen.queryByText(/50kg/i);
+
+  expect(name).toBeVisible();
+  expect(type).toBeVisible();
+  expect(dexEntry).toBeVisible();
+  expect(height).toBeVisible();
+  expect(weight).toBeVisible();
+});
