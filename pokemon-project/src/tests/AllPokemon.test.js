147948987209236d30ca6a995b20e75d31801391
adamhinton <adam.d.hinton@gmail.com>
2022-01-01 12:16:05 -0500

    Added testing to AllPokemon.test to make sure it can render multiple pokemon

--- pokemon-project/src/tests/AllPokemon.test.js
+++ pokemon-project/src/tests/AllPokemon.test.js
@@ -16,6 +16,34 @@ const fakePokemon = [
   },
 ];
 
+//This is similar to the above array but with multiple pokemon for testing purposes
+const multFakePokemon = [
+  {
+    name: "Fake Pokemon Name",
+    type: "Death Type",
+    img: "https://google.com",
+    dexEntry: "blah blah blah",
+    height: "4m",
+    weight: "50kg",
+  },
+  {
+    name: "Fake2",
+    type: "Derp Type",
+    img: "https://google.com/1234",
+    dexEntry: "This pokemon does nothing",
+    height: "5m",
+    weight: "60kg",
+  },
+  {
+    name: "Fake3",
+    type: "Ground Type",
+    img: "https://google.com/1234567",
+    dexEntry: "This pokemon is god",
+    height: "6m",
+    weight: "80kg",
+  },
+];
+
 test("AllPokemon renders without errors", () => {
   render(<AllPokemon pokemon={fakePokemon} />);
 });
@@ -31,4 +59,16 @@ test("AllPokemon renders correctly with certain props, then rerenders when those
   const newFakeName = screen.queryByText(/fake pokemon name/i);
 
   expect(newFakeName).toBeInTheDocument();
+});
+
+test("Can render multiple pokemon", () => {
+  render(<AllPokemon pokemon={multFakePokemon} />);
+
+  const name1 = screen.queryByText(/fake pokemon name/i);
+  const name2 = screen.queryByText(/fake2/i);
+  const name3 = screen.queryByText(/fake3/i);
+
+  expect(name1).toBeVisible();
+  expect(name2).toBeVisible();
+  expect(name3).toBeVisible();
 });
