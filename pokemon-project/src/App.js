f400840096a0936f64c8c249c491f414c330ed9d
adamhinton <adam.d.hinton@gmail.com>
2022-01-07 10:34:34 -0500

    Cleaned up comments, trimmed white space in App.js

--- pokemon-project/src/App.js
+++ pokemon-project/src/App.js
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
-
-//importing components
 import Header from "./components/Header";
 import AllPokemon from "./components/AllPokemon";
 
-//This is the original data of the 151 pokemon I'm displaying. It gets updated and passed to state based on what display options the user clicks.
+//This is the original data of the 151 pokemon I'm displaying.
 import data from "./data";
 
 function App() {
-  //sets the pokemon data to state. It starts as the original data but my hope is that I can update it based on filters.
+  //sets the pokemon data to state. Updates based on user preference
   const [pokemon, setPokemon] = useState(data);
-  // This is a boolean that shows either the shiny or normal image of a pokemon based on user selection in header. Gets passed in to header and AllPokemon (through to SinglePokemon)
+  // Boolean that user can update via button clickto display shiny forms of pokemon
   const [isShiny, setIsShiny] = useState(false);
 
-  //This function is the heart of the entire project. It allows you to press a button in the header dropdown to show only pokemon of a certain type, height, or size.
-  //If a type is passed in, it sorts by that type. If a size is passed in, it sorts by that size. If a height is passed in, it osrts by that size. Nothing else will ever be passed in unless the code is written with more button options.
+  //Allows the user to press a button in the header dropdown to show only pokemon of a certain type, height, or size.
+  //Sorts by type, height, or size based on what the user clicks
   const changeDisplay = (type = null, size = null, height = null) => {
     const newData = data.filter((item) => {
       if (type) {
@@ -26,7 +24,6 @@ function App() {
         const heightInt = item.height.split(" ")[0];
         return height === "short" ? heightInt < 0.4 : heightInt > 2;
       }
-      //I don't think returning null is necessary here but VSCode gave me a warning until I did it.
       return null;
     });
     setPokemon(newData);
