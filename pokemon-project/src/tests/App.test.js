53b77ac9dee8087132b128c6edb1c0f290cf5df5
adamhinton <adam.d.hinton@gmail.com>
2022-01-10 23:39:56 -0500

    added sanity check to App.test.js

--- pokemon-project/src/tests/App.test.js
+++ pokemon-project/src/tests/App.test.js
@@ -5,6 +5,12 @@ import App from "../App";
 
 //I've ensured these tests are falsifiable by altering variables and expects
 
+test("Sanity Check", () => {
+  const twoPlusTwo = 2 + 2;
+  expect(twoPlusTwo).toStrictEqual(4);
+  expect(twoPlusTwo).not.toStrictEqual(5);
+});
+
 test("renders without errors", () => {
   render(<App />);
 });
