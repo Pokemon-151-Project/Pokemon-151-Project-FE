dc1fce8e98c54a939c81d926597f454af7b11568
adamhinton <adam.d.hinton@gmail.com>
2022-03-22 17:23:28 -0400

    Put typesArray array in utils/dropdown/typesArray to keep Dropdown component cleaner

---
+++ pokemon-project/src/utils/Dropdownutils/typesArray.js
@@ -0,0 +1,19 @@
+const typesArray = [
+  "Normal",
+  "Fire",
+  "Water",
+  "Electric",
+  "Grass",
+  "Ice",
+  "Fighting",
+  "Poison",
+  "Ground",
+  "Flying",
+  "Psychic",
+  "Bug",
+  "Rock",
+  "Ghost",
+  "Dragon",
+];
+
+export default typesArray;
