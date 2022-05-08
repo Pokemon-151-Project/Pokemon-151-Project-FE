f1050fbefa6761f26350feb3f52ed5f6f9b73711
adamhinton <adam.d.hinton@gmail.com>
2022-05-08 00:33:57 -0400

    move two tests from Dropdown to App.test

--- src/tests/Dropdown.test.js
+++ src/tests/Dropdown.test.js
@@ -15,35 +15,3 @@ test("[2] All dropdown buttons render", () => {
   const buttons = screen.queryAllByTestId("dropbtn-inner");
   expect(buttons).toBeTruthy();
 });
-
-test("[3] Dropdown buttons change the displayed pokemon", async () => {
-  // I had to render App for this because it doesn't just test dropdown. Not sure what best practices is for this but it does the job.
-  render(<App />);
-
-  const fireBtn = document.querySelector("#fire-btn");
-  let bulba = screen.queryByText(/bulbasaur/i);
-  expect(bulba).toBeVisible();
-
-  await userEvent.click(fireBtn);
-  expect(bulba).not.toBeVisible();
-});
-
-// This test hits the button to only show water types, verifies that non-water types don't display, then hits the reset button, then verifies that all types now display again as expected.
-test("[4] Reset buttons puts all pokemon back on screen", async () => {
-  render(<App />);
-
-  const waterBtn = screen.getByTestId("water-btn");
-  const resetBtn = screen.getByTestId("reset-btn");
-  const bulba = screen.getByText(/bulbasaur/i);
-  expect(bulba).toBeVisible();
-
-  await userEvent.click(waterBtn);
-  expect(bulba).not.toBeVisible();
-
-  const starmie = screen.getByText(/starmie/i);
-  expect(starmie).toBeVisible();
-
-  await userEvent.click(resetBtn);
-  const chariz = screen.getByText(/charizard/i);
-  expect(chariz).toBeVisible();
-});
