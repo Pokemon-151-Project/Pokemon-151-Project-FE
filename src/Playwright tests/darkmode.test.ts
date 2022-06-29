14958f1db407bc84873e363f7c080ec81c0f22f1
adamhinton <adam.d.hinton@gmail.com>
2022-06-29 11:20:40 -0400

    add beforeEach page.goto

--- src/Playwright tests/darkmode.test.ts
+++ src/Playwright tests/darkmode.test.ts
@@ -1,9 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Dark Mode Tests", () => {
-  test("Elements have lightmode CSS by default", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:3000/");
+  });
 
+  test("Elements have lightmode CSS by default", async ({ page }) => {
     await expect(
       page.locator(
         "text=Bulbasaur #1Grass / Poison Height: 0.71 m Weight: 6.9 kgA strange seed was plant"
