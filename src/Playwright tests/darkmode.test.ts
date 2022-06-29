d6def3cbf551d0fe8b48e2d979bf0338c7d545e3
adamhinton <adam.d.hinton@gmail.com>
2022-06-29 11:19:00 -0400

    write first basic darkmode test

--- src/Playwright tests/darkmode.test.ts
+++ src/Playwright tests/darkmode.test.ts
@@ -1,7 +1,14 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Dark Mode Tests", () => {
-  test("Test123", () => {
-    console.log("blah blah blah");
+  test("Elements have lightmode CSS by default", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    await expect(
+      page.locator(
+        "text=Bulbasaur #1Grass / Poison Height: 0.71 m Weight: 6.9 kgA strange seed was plant"
+      )
+      // It's #bef8f8 in my code but the test runner interprets it as this rgb color
+    ).toHaveCSS("background-color", "rgb(190, 248, 248)");
   });
 });
