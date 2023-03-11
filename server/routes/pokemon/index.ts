91b80b9807107fcb5f3994e9b44cf95511bc546f
adamhinton <adam.d.hinton@gmail.com>
2023-03-11 18:55:14 -0500

    delete unused code

--- server/routes/pokemon/index.ts
+++ server/routes/pokemon/index.ts
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-import { validatePokemon } from "../../middleware/Pokemon/pokemonMiddleware";
 import { getAllPokemon } from "../controllers/pokemon";
 
 router.get("/", getAllPokemon);
