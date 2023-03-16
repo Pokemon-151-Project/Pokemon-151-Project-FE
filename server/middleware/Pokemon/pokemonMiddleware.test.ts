a41bdd051c67f7b0f64ba4508c1054d2535eaac7
adamhinton <adam.d.hinton@gmail.com>
2023-03-16 18:35:22 -0400

    first successful pokemonMiddleware test

--- server/middleware/Pokemon/pokemonMiddleware.test.ts
+++ server/middleware/Pokemon/pokemonMiddleware.test.ts
@@ -2,45 +2,65 @@ import { NextFunction } from "express";
 import { validatePokemon } from "./pokemonMiddleware";
 var httpMocks = require("node-mocks-http");
 
-test("test_validArray", async () => {
-	const bob = "bob";
+test("[1] Sanity check", async () => {
+	const bob = 2;
+	expect(bob).toBe(2);
+});
 
-	// const req: Request = {
-	// 	body: {
-	// 		pokemonList: [
-	// 			{
-	// 				dexID: 8,
-	// 				name: "Pikasaurmon",
-	// 				num: "003",
-	// 				primaryType: "Pikatype",
-	// 				secondaryType: "Bulbatype",
-	// 				height: "0.5m",
-	// 				weight: "66kg",
-	// 				dexEntry: "Pika pika!",
-	// 			},
-	// 			{
-	// 				dexID: 25,
-	// 				name: "Pikachu",
-	// 				num: "025",
-	// 				primaryType: "Electric",
-	// 				secondaryType: null,
-	// 				height: "0.4m",
-	// 				weight: "6kg",
-	// 				dexEntry:
-	// 					"When several of these Pokémon gather, their electricity could build and cause lightning storms.",
-	// 			},
-	// 		],
+test("test_validPokemonList", async () => {
+	// const validPokemonList = [
+	// 	{
+	// 		dexID: 8,
+	// 		name: "Pikasaurmon",
+	// 		num: "003",
+	// 		primaryType: "Pikatype",
+	// 		secondaryType: "Bulbatype",
+	// 		height: "0.5m",
+	// 		weight: "66kg",
+	// 		dexEntry: "Pika pika!",
 	// 	},
-	// } as any as Request;
-	// const res: Response = {
-	// 	status: jest.fn().mockReturnThis(),
-	// 	json: jest.fn().mockReturnThis(),
-	// 	send: jest.fn().mockReturnThis(),
-	// } as any as Response;
-	// const next: NextFunction = jest.fn();
-	// await validatePokemon(req, res, next);
-	// expect(next).toHaveBeenCalled();
-	// expect(res.status).not.toHaveBeenCalled();
-	// expect(res.json).not.toHaveBeenCalled();
-	// expect(res.status).not.toHaveBeenCalled();
+	// 	{
+	// 		dexID: 25,
+	// 		name: "Pikachu",
+	// 		num: "025",
+	// 		primaryType: "Electric",
+	// 		secondaryType: null,
+	// 		height: "0.4m",
+	// 		weight: "6kg",
+	// 		dexEntry:
+	// 			"When several of these Pokémon gather, their electricity could build and cause lightning storms.",
+	// 	},
+	// ];
+	const req: Request = httpMocks.createRequest({
+		body: {
+			pokemonList: [
+				{
+					dexID: 12,
+					name: "Pikasaurmon",
+					num: "003",
+					primaryType: "Pikatype",
+					secondaryType: "Bulbatype",
+					height: "0.5m",
+					weight: "66kg",
+					dexEntry: "Pika pika!",
+				},
+				{
+					dexID: 54,
+					name: "Pikasaurmon",
+					num: "003",
+					primaryType: "Pikatype",
+					secondaryType: "Bulbatype",
+					height: "0.5m",
+					weight: "66kg",
+					dexEntry: "Pika pika!",
+				},
+			],
+		},
+	});
+	const res: Response = httpMocks.createResponse();
+	const next: NextFunction = jest.fn();
+	// @ts-ignore
+	await validatePokemon(req, res, next);
+
+	expect(next).toHaveBeenCalled();
 });
