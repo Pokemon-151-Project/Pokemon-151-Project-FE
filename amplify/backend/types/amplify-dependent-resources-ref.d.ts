2ef4159cdd2380cb483cdffd229273dec787e84d
adamhinton <adam.d.hinton@gmail.com>
2023-08-05 11:49:59 -0400

    npx amplify push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "server": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
---
+++ src/graphql/mutations.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createPokemon = /* GraphQL */ `
+  mutation CreatePokemon(
+    $input: CreatePokemonInput!
+    $condition: ModelPokemonConditionInput
+  ) {
+    createPokemon(input: $input, condition: $condition) {
+      id
+      num
+      name
+      type
+      height
+      weight
+      dexEntry
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updatePokemon = /* GraphQL */ `
+  mutation UpdatePokemon(
+    $input: UpdatePokemonInput!
+    $condition: ModelPokemonConditionInput
+  ) {
+    updatePokemon(input: $input, condition: $condition) {
+      id
+      num
+      name
+      type
+      height
+      weight
+      dexEntry
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deletePokemon = /* GraphQL */ `
+  mutation DeletePokemon(
+    $input: DeletePokemonInput!
+    $condition: ModelPokemonConditionInput
+  ) {
+    deletePokemon(input: $input, condition: $condition) {
+      id
+      num
+      name
+      type
+      height
+      weight
+      dexEntry
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
