30fc93084a2182e8922abeab4f4d0b1c3c098fd9
adamhinton <adam.d.hinton@gmail.com>
2023-08-26 11:59:16 -0400

    Adjust display options copy

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,112 +6,112 @@
 import typesArray from "../utils/Dropdownutils/typesArray";
 
 const Dropdown = (props) => {
-  const { changeDisplay } = props;
+	const { changeDisplay } = props;
 
-  return (
-    <section className="dropdown">
-      {/* Hovering here shows the dropdown menu with display options */}
-      <button className="dropbtn" data-testid="dropbtn">
-        Display Options (check this out!)
-      </button>
+	return (
+		<section className="dropdown">
+			{/* Hovering here shows the dropdown menu with display options */}
+			<button className="dropbtn" data-testid="dropbtn">
+				Display Options
+			</button>
 
-      <menu className="dropdown-content">
-        <div>
-          <div>
-            {/* Reset to original display options by refreshing the page*/}
-            <button
-              className="dropbtn-inner"
-              data-testid="reset-btn"
-              id="reset-btn"
-              onClick={() => {
-                changeDisplay(null, null, null, "reset");
-              }}
-            >
-              Reset All
-            </button>
-          </div>
-          <h5
-            className="dropdown-display-title"
-            data-testid="dropdown-display-title"
-          >
-            Pick a type:
-          </h5>
-          {/* Returns 15 buttons, one for each Pokemon type*/}
-          {typesArray.map((item) => {
-            return (
-              <button
-                className="dropbtn-inner"
-                data-testid="dropbtn-inner"
-                id={`${item.toLowerCase()}-btn`}
-                key={item}
-                onClick={() => {
-                  changeDisplay(item);
-                }}
-              >
-                {item}
-              </button>
-            );
-          })}
-        </div>
+			<menu className="dropdown-content">
+				<div>
+					<div>
+						{/* Reset to original display options by refreshing the page*/}
+						<button
+							className="dropbtn-inner"
+							data-testid="reset-btn"
+							id="reset-btn"
+							onClick={() => {
+								changeDisplay(null, null, null, "reset");
+							}}
+						>
+							Reset All
+						</button>
+					</div>
+					<h5
+						className="dropdown-display-title"
+						data-testid="dropdown-display-title"
+					>
+						Pick a type:
+					</h5>
+					{/* Returns 15 buttons, one for each Pokemon type*/}
+					{typesArray.map((item) => {
+						return (
+							<button
+								className="dropbtn-inner"
+								data-testid="dropbtn-inner"
+								id={`${item.toLowerCase()}-btn`}
+								key={item}
+								onClick={() => {
+									changeDisplay(item);
+								}}
+							>
+								{item}
+							</button>
+						);
+					})}
+				</div>
 
-        {/* Begin buttons for sorting pokemon by weight */}
-        <div>
-          {/* Button to show only small pokemon (<10kg) */}
-          <h5 className="dropdown-display-title">Pick a weight:</h5>
-          <button
-            className="dropbtn-inner"
-            data-testid="dropbtn-inner"
-            id="small-btn"
-            onClick={() => {
-              changeDisplay(null, "small");
-            }}
-          >
-            Small
-          </button>
+				{/* Begin buttons for sorting pokemon by weight */}
+				<div>
+					{/* Button to show only small pokemon (<10kg) */}
+					<h5 className="dropdown-display-title">Pick a weight:</h5>
+					<button
+						className="dropbtn-inner"
+						data-testid="dropbtn-inner"
+						id="small-btn"
+						onClick={() => {
+							changeDisplay(null, "small");
+						}}
+					>
+						Small
+					</button>
 
-          {/* Button to show only big pokemon (>100kg) */}
-          <button
-            className="dropbtn-inner"
-            data-testid="dropbtn-inner"
-            id="big-btn"
-            onClick={() => {
-              changeDisplay(null, "big");
-            }}
-          >
-            Big
-          </button>
-        </div>
+					{/* Button to show only big pokemon (>100kg) */}
+					<button
+						className="dropbtn-inner"
+						data-testid="dropbtn-inner"
+						id="big-btn"
+						onClick={() => {
+							changeDisplay(null, "big");
+						}}
+					>
+						Big
+					</button>
+				</div>
 
-        {/* Begin buttons for sorting pokemon by height */}
-        <div>
-          <h5 className="dropdown-display-title">Pick a height:</h5>
-          {/* Show only short pokemon */}
-          <button
-            className="dropbtn-inner"
-            data-testid="dropbtn-inner"
-            id="short-btn"
-            onClick={() => {
-              changeDisplay(null, null, "short");
-            }}
-          >
-            Short
-          </button>
+				{/* Begin buttons for sorting pokemon by height */}
+				<div>
+					<h5 className="dropdown-display-title">Pick a height:</h5>
+					{/* Show only short pokemon */}
+					<button
+						className="dropbtn-inner"
+						data-testid="dropbtn-inner"
+						id="short-btn"
+						onClick={() => {
+							changeDisplay(null, null, "short");
+						}}
+					>
+						Short
+					</button>
 
-          {/* Button to show only tall pokemon */}
-          <button
-            className="dropbtn-inner"
-            data-testid="dropbtn-inner"
-            id="tall-btn"
-            onClick={() => {
-              changeDisplay(null, null, "tall");
-            }}
-          >
-            Tall
-          </button>
-        </div>
-      </menu>
-    </section>
-  );
+					{/* Button to show only tall pokemon */}
+					<button
+						className="dropbtn-inner"
+						data-testid="dropbtn-inner"
+						id="tall-btn"
+						onClick={() => {
+							changeDisplay(null, null, "tall");
+						}}
+					>
+						Tall
+					</button>
+				</div>
+			</menu>
+		</section>
+	);
 };
 
 export default Dropdown;
