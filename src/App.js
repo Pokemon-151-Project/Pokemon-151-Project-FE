d7d16e4fa5b8524995beb242d1f66247535e1ed1
adamhinton <adam.d.hinton@gmail.com>
2023-09-24 11:33:35 -0400

    add tsconfig and update imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
-import AllPokemon from "./components/AllPokemon";
+import Header from "./components/Header.tsx";
+import AllPokemon from "./components/AllPokemon.tsx";
 //This is the original data of the 151 pokemon being displayed.
-import data from "./data";
+import data from "./data.ts";
 
 function App() {
 	//sets the pokemon data to state. Updates based on user preference
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,63 +10,63 @@ import linksArray from "../utils/Headerutils/linksArray";
 import linkMaker from "../utils/Headerutils/linkMaker";
 
 const Header = (props) => {
-  const [isDarkMode, setDarkMode] = useDarkMode();
+	const [isDarkMode, setDarkMode] = useDarkMode();
 
-  const { changeDisplay, isShiny, setIsShiny } = props;
-  return (
-    <header>
-      <h1 className="header-item header-h1">Pokemon 151 Project</h1>
+	const { changeDisplay, isShiny, setIsShiny } = props;
+	return (
+		<header>
+			<h1 className="header-item header-h1">Pokemon 151 Project</h1>
 
-      {/* Begin display options dropdown. See Dropdown.js */}
-      <section className="darkmode-shiny-dropdown">
-        <menu className="darkmode-shiny-section">
-          <div className="darkmode-section">
-            <div className="logo" data-testid="logo">
-              {isDarkMode ? "Dark Mode" : "Light Mode"}
-            </div>
-            <button
-              className="toggle_btn"
-              data-testid="toggle_btn"
-              onClick={() => {
-                setDarkMode(!isDarkMode);
-              }}
-            >
-              {isDarkMode ? (
-                <BsSun color="#ff0" size="24" title="Switch to light mode" />
-              ) : (
-                <BsMoon size="24" title="Switch to dark mode" />
-              )}
-            </button>
-          </div>
+			{/* Begin display options dropdown. See Dropdown.js */}
+			<section className="darkmode-shiny-dropdown">
+				<menu className="darkmode-shiny-section">
+					<div className="darkmode-section">
+						<div className="logo" data-testid="logo">
+							{isDarkMode ? "Dark Mode" : "Light Mode"}
+						</div>
+						<button
+							className="toggle_btn"
+							data-testid="toggle_btn"
+							onClick={() => {
+								setDarkMode(!isDarkMode);
+							}}
+						>
+							{isDarkMode ? (
+								<BsSun color="#ff0" size="24" title="Switch to light mode" />
+							) : (
+								<BsMoon size="24" title="Switch to dark mode" />
+							)}
+						</button>
+					</div>
 
-          {/* This is the styled big green line running through the header */}
-          <div className="vertical-line" />
+					{/* This is the styled big green line running through the header */}
+					<div className="vertical-line" />
 
-          {/* This button toggles whether the app shows the Shiny forms of the pokemon */}
-          <div className="shiny-button-div">
-            <button
-              className="shiny-button"
-              onClick={() => {
-                setIsShiny(!isShiny);
-              }}
-            >
-              {isShiny ? "Show Non-Shiny Forms" : "Show Shiny Forms"}
-            </button>
-          </div>
-        </menu>
+					{/* This button toggles whether the app shows the Shiny forms of the pokemon */}
+					<div className="shiny-button-div">
+						<button
+							className="shiny-button"
+							onClick={() => {
+								setIsShiny(!isShiny);
+							}}
+						>
+							{isShiny ? "Show Non-Shiny Forms" : "Show Shiny Forms"}
+						</button>
+					</div>
+				</menu>
 
-        {/* The dropdown section lives here */}
-        <Dropdown changeDisplay={changeDisplay} />
-      </section>
+				{/* The dropdown section lives here */}
+				<Dropdown changeDisplay={changeDisplay} />
+			</section>
 
-      <section className="adam-info header-item">
-        <h2>Author: Adam Hinton</h2>
+			<section className="adam-info header-item">
+				<h2>Author: Adam Hinton</h2>
 
-        {/* the linkMaker function produces header links. Update linksArray (imported above) to add/change/remove links*/}
-        <nav className="header-nav">{linkMaker(linksArray)}</nav>
-      </section>
-    </header>
-  );
+				{/* the linkMaker function produces header links. Update linksArray (imported above) to add/change/remove links*/}
+				<nav className="header-nav">{linkMaker(linksArray)}</nav>
+			</section>
+		</header>
+	);
 };
 
 export default Header;
