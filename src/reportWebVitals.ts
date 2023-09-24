b2a259ef60c743a549b0e38a64c866fd5d1292bd
adamhinton <adam.d.hinton@gmail.com>
2023-09-24 15:27:56 -0400

    convert reportWebVitals to ts. All tests pass

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,15 @@
+type ReportHandler = (metric: any) => void;
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+	if (onPerfEntry && typeof onPerfEntry === "function") {
+		import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+			getCLS(onPerfEntry);
+			getFID(onPerfEntry);
+			getFCP(onPerfEntry);
+			getLCP(onPerfEntry);
+			getTTFB(onPerfEntry);
+		});
+	}
+};
+
+export default reportWebVitals;
