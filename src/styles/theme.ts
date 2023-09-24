e3e9022093d8c3038c84d0abe0ae6b0376b957e9
adamhinton <adam.d.hinton@gmail.com>
2023-09-24 19:01:28 -0400

    skeleton GlobalStyles and themes files

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+// theme.ts
+export const lightTheme = {
+	bodyBackgroundColor: "#3c9893",
+	// Add more light theme variables here
+};
+
+export const darkTheme = {
+	bodyBackgroundColor: "rgb(45, 60, 66)",
+	// Add more dark theme variables here
+};
