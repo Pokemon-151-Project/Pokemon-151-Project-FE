1ab493aa2e1c9e244f44a0b683c40bd2b5570eb2
adamhinton <adam.d.hinton@gmail.com>
2023-09-24 11:00:09 -0400

    delete amplify stuff

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "server": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
