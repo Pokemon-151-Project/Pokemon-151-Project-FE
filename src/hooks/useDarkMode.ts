0c4e424272bde3004db8f971bc1147deccd91d8e
adamhinton <adam.d.hinton@gmail.com>
2023-09-24 23:40:01 -0400

    delete CSS class manipulation from useDarkMode(

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -11,30 +11,16 @@ const useDarkMode = (): [
 			const item = window.localStorage.getItem(key);
 			return item ? JSON.parse(item) : false;
 		} catch (error) {
+			console.error("Error reading from local storage:", error);
 			return false;
 		}
 	});
 
 	useEffect(() => {
-		const className = "dark";
-		const pokeCards = window.document.querySelectorAll(".pokemon-card");
-
-		if (isDark) {
-			window.document.body.classList.add(className);
-			pokeCards.forEach((item) => {
-				item.classList.add(className);
-			});
-		} else {
-			window.document.body.classList.remove(className);
-			pokeCards.forEach((item) => {
-				item.classList.remove(className);
-			});
-		}
-
 		try {
 			window.localStorage.setItem(key, JSON.stringify(isDark));
 		} catch (e) {
-			console.error("Error in setting preference");
+			console.error("Error in setting preference:", e);
 		}
 	}, [isDark]);
 
