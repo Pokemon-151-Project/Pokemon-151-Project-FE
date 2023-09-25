e4cc70c3c52947fb939b343e39b507da6455c2ce
adamhinton <adam.d.hinton@gmail.com>
2023-09-24 23:34:57 -0400

    styling AllPokemon

--- src/components/AllPokemon.tsx
+++ src/components/AllPokemon.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import SinglePokemon from "./SinglePokemon";
 import { Pokemon } from "../data";
+
+const AllPokemonContainer = styled.main`
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: row;
+	justify-content: center;
+	padding: 2%;
+	background-color: ${({ theme }) => theme.allPokemonBackground};
+`;
 
 type Props = {
 	pokemonData: Pokemon[];
@@ -9,7 +19,7 @@ type Props = {
 
 const AllPokemon: React.FC<Props> = ({ pokemonData, isShiny }) => {
 	return (
-		<main className="all-pokemon">
+		<AllPokemonContainer>
 			{pokemonData.map((poke) => (
 				<SinglePokemon
 					poke={poke}
@@ -18,7 +28,7 @@ const AllPokemon: React.FC<Props> = ({ pokemonData, isShiny }) => {
 					isShiny={isShiny}
 				/>
 			))}
-		</main>
+		</AllPokemonContainer>
 	);
 };
 
