8b1cb341d5c4c67bbfa3246b1618f29a91103a74
adamhinton <adam.d.hinton@gmail.com>
2023-10-01 19:29:11 -0400

    update initial-data-population to delete old data and add new stuff

--- src/utils/initial-data-population.ts
+++ src/utils/initial-data-population.ts
@@ -1,12 +1,41 @@
 import { Amplify, API, graphqlOperation } from "aws-amplify";
-import { createPokemon } from ".././graphql/mutations";
+import { createPokemon, deletePokemon } from "../graphql/mutations"; // Import deletePokemon
+import { listPokemon } from "../graphql/queries"; // Import listPokemon
 import pokemonData from "../data";
 import awsconfig from "../aws-exports"; // adjust the path
+import { ListPokemonQuery } from "../API";
 
 Amplify.configure(awsconfig);
 
+const deleteExistingData = async () => {
+	try {
+		const { data } = (await API.graphql(graphqlOperation(listPokemon))) as {
+			data: ListPokemonQuery;
+		};
+		const existingPokemon = data?.listPokemon?.items;
+
+		if (existingPokemon) {
+			for (const pokemon of existingPokemon) {
+				if (pokemon?.id) {
+					await API.graphql(
+						graphqlOperation(deletePokemon, { input: { id: pokemon.id } })
+					);
+					console.log(`Successfully deleted ${pokemon.name}`);
+				}
+			}
+		}
+	} catch (error) {
+		console.error("Error deleting existing records:", error);
+	}
+};
+
 const populateData = async () => {
-	for (const pokemon of pokemonData) {
+	// Sort pokemonData by Pokedex number
+	const sortedPokemonData = [...pokemonData].sort(
+		(a, b) => parseInt(a.num) - parseInt(b.num)
+	);
+
+	for (const pokemon of sortedPokemonData) {
 		try {
 			await API.graphql(graphqlOperation(createPokemon, { input: pokemon }));
 			console.log(`Successfully added ${pokemon.name}`);
@@ -16,4 +45,9 @@ const populateData = async () => {
 	}
 };
 
-populateData();
+const run = async () => {
+	await deleteExistingData();
+	await populateData();
+};
+
+run();
