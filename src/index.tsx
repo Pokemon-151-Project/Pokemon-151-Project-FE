f90c5b3f321ec1c38562e6041195d7a7c089e7ca
adamhinton <adam.d.hinton@gmail.com>
2023-10-01 14:48:23 -0400

    Configure Amplify in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Amplify } from "aws-amplify";
+import awsmobile from "./aws-exports";
+
+Amplify.configure(awsmobile);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
